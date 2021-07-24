0f27000af705891a400cc8c7d6c58b10c086155e
BuD <15681801+BuDMan1980@users.noreply.github.com>
2021-07-24 01:59:55 +0000

    Initial commit

---
+++ Basic JS/01-comments.js
@@ -0,0 +1,13 @@
+// COMMENTS: Why use them?
+
+// Use comments to explain in a clear way,
+// what each section of code is suppose to
+// be for. That way, when other people need
+// to make changes they'll be able to find
+// the correct piece of code they're looking
+// for. It'll also help you remember what
+// you made, if you leave it and come back
+// to work on it later.
+
+// This is a comment and will not be interpreted as code.
+console.log("The comment is not code. YAYYY!!!");
---
+++ Basic JS/02-variables.js
@@ -0,0 +1,19 @@
+/*
+	Cannot be a reserved keyword. These are keywords that are in JS like, let, var, if, etc.
+	Should be meaningful, not just letters like a, b, x, etc.
+	Cannot start with a number.
+	Cannot contain space.
+*/
+
+// Using only the variable name without any space.
+let name;
+
+// Using two words separated by an underscore.
+let my_name;
+
+// Using two words using camelCase(First letter capital of each word except first.)
+let myName;
+
+// Using a word and a number without any space.
+let name1
+
