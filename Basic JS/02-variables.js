66a77634bb43f6ac34ddb8b90c6531324ba7b1af
BuD <15681801+BuDMan1980@users.noreply.github.com>
2021-07-24 02:16:20 +0000

    Working on the variables page

--- Basic JS/02-variables.js
+++ Basic JS/02-variables.js
@@ -1,9 +1,15 @@
+// VARIABLES
+
 /*
-	Cannot be a reserved keyword. These are keywords that are in JS like, let, var, if, etc.
-	Should be meaningful, not just letters like a, b, x, etc.
-	Cannot start with a number.
-	Cannot contain space.
+Things to remember when using variables:
+
+	*	Cannot be a reserved keyword. These are keywords that are in JS like, let, var, if, etc.
+	*	Should be meaningful, not just letters like a, b, x, etc.
+	*	Cannot start with a number.
+	*	Cannot contain space.
 */
+
+/* Ways to create a variable (without a value) */
 
 // Using only the variable name without any space.
 let name;
@@ -15,5 +21,14 @@ let my_name;
 let myName;
 
 // Using a word and a number without any space.
-let name1
+let name1;
 
+/* Using a variable without giving it a value will show up as 'undefined' */
+console.log(name);
+console.log(my_name);
+console.log(myName);
+console.log(name1);
+
+// Now we give the variable a value
+let name = "John";
+console.log(name);
